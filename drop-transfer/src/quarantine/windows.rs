cfbdc67eca708ff8f54b77d5ef6a9b18574b4e48
Mateusz Szczygiel <mateusz.szczygiel@nordsec.com>
2023-03-14 10:10:29 +0200

    Fix unit tests

--- drop-transfer/src/quarantine/windows.rs
+++ drop-transfer/src/quarantine/windows.rs
@@ -4,7 +4,9 @@ use std::{
     path::Path,
 };
 
-impl super::PathExt for Path {
+use super::PathExt;
+
+impl PathExt for Path {
     fn quarantine(&self) -> Result<()> {
         if let Some(name) = self.file_name() {
             let mut name = name.to_os_string();
@@ -27,15 +29,17 @@ impl super::PathExt for Path {
 
 #[cfg(all(test, windows))]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
     fn write_zone_identifier() -> Result<()> {
-        let file = NamedTempFile::new()?;
+        let file = tempfile::NamedTempFile::new()?;
         let path = file.path();
         let name = path.file_name();
 
-        assert(name.is_some());
+        assert!(name.is_some());
 
         path.quarantine()?;
 
@@ -43,7 +47,7 @@ mod tests {
 
         name.push(":Zone.Identifier:$DATA");
 
-        assert(fs::read_to_string(path.with_file_name(name))? == "[ZoneTransfer]\nZoneId=3");
+        assert!(fs::read_to_string(path.with_file_name(name))? == "[ZoneTransfer]\nZoneId=3");
 
         Ok(())
     }
