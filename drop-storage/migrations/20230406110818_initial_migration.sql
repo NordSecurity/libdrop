fb340f9fcf633aceb3b0a8e545f9fad536ba0cd9
Martynas Gurskas <martynas.gurskas@nordsec.com>
2023-05-09 12:40:42 +0300

    Update database structure/migration
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- drop-storage/migrations/20230406110818_initial_migration.sql
+++ drop-storage/migrations/20230406110818_initial_migration.sql
@@ -14,24 +14,15 @@ CREATE TABLE IF NOT EXISTS transfers (
   CHECK(is_outgoing = 0 OR is_outgoing = 1)
 );
 
--- added paths to the outgoing transfer
-CREATE TABLE IF NOT EXISTS outgoing_paths (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  transfer_id TEXT NOT NULL, 
-  path TEXT NOT NULL, 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
 -- traversed paths for outgoing transfer at the time of the transfer creation
 -- this is used to "freeze" the state of the files so we could choose to either traverse again or use the traversed paths as origin for the transfer
-CREATE TABLE IF NOT EXISTS outgoing_traversed_paths (
+CREATE TABLE IF NOT EXISTS outgoing_paths (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  path_id INTEGER NOT NULL, 
+  transfer_id INTEGER NOT NULL,
+  path TEXT NOT NULL,
   bytes INT NOT NULL, 
-  full_path TEXT NOT NULL, 
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- all the paths inside the incoming transfer
@@ -45,43 +36,43 @@ CREATE TABLE IF NOT EXISTS incoming_paths (
 );
 
 -- states for outgoing paths(uploads)
-CREATE TABLE IF NOT EXISTS outgoing_traversed_path_pending_states (
+CREATE TABLE IF NOT EXISTS outgoing_path_pending_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_traversed_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-CREATE TABLE IF NOT EXISTS outgoing_traversed_path_started_states (
+CREATE TABLE IF NOT EXISTS outgoing_path_started_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id TEXT NOT NULL, 
   bytes_sent INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_traversed_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
-CREATE TABLE IF NOT EXISTS outgoing_traversed_path_cancel_states (
+CREATE TABLE IF NOT EXISTS outgoing_path_cancel_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id TEXT NOT NULL, 
   by_peer INTEGER NOT NULL,
   bytes_sent INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_traversed_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
-CREATE TABLE IF NOT EXISTS outgoing_traversed_path_failed_states (
+CREATE TABLE IF NOT EXISTS outgoing_path_failed_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id TEXT NOT NULL, 
   status_code INTEGER NOT NULL,
   bytes_sent INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_traversed_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
-CREATE TABLE IF NOT EXISTS outgoing_traversed_path_completed_states (
+CREATE TABLE IF NOT EXISTS outgoing_path_completed_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id TEXT NOT NULL,   
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  FOREIGN KEY(path_id) REFERENCES outgoing_traversed_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- states for incoming paths(downloads)
@@ -101,7 +92,7 @@ CREATE TABLE IF NOT EXISTS incoming_transfer_path_started_states (
 );
 CREATE TABLE IF NOT EXISTS incoming_transfer_path_cancel_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  path_id TEXT NOT NULL, 
+  path_id TEXT NOT NULL,
   by_peer INTEGER NOT NULL,
   bytes_received INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
