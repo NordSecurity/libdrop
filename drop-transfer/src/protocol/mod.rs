e47f99fb9d17c4f0f479d9c7c740c93fb3bedb4b
Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>
2023-05-11 16:14:49 +0300

    V4 protocol structures
    Signed-off-by: Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>

--- drop-transfer/src/protocol/mod.rs
+++ drop-transfer/src/protocol/mod.rs
@@ -3,6 +3,7 @@ pub mod v2 {
     pub use super::v1::*;
 }
 pub mod v3;
+pub mod v4;
 
 #[derive(Copy, Clone, strum::Display, strum::EnumString)]
 pub enum Version {
@@ -12,4 +13,6 @@ pub enum Version {
     V2,
     #[strum(serialize = "v3")]
     V3,
+    // #[strum(serialize = "v4")]
+    // V4,
 }
