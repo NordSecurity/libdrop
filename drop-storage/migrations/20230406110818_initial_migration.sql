129bcd2750f442b6416e0668e7ee4355f0723123
Martynas Gurskas <martynas.gurskas@nordsec.com>
2023-05-17 15:03:14 +0300

    Add milliseconds to default timestamp values This should fix the problem of not being able to restore the order of events from the database when they are fired too quickly when inserting
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- drop-storage/migrations/20230406110818_initial_migration.sql
+++ drop-storage/migrations/20230406110818_initial_migration.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS peers (
   id TEXT PRIMARY KEY UNIQUE NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))
 );
 
 --- outgoing transfers
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS transfers (
   id TEXT PRIMARY KEY UNIQUE NOT NULL, 
   peer_id TEXT NOT NULL, 
   is_outgoing INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(peer_id) REFERENCES peers(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(is_outgoing = 0 OR is_outgoing = 1)
 );
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS transfers (
 CREATE TABLE IF NOT EXISTS transfer_active_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   transfer_id TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS transfer_cancel_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   transfer_id TEXT NOT NULL,
   by_peer INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS transfer_failed_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   transfer_id TEXT NOT NULL,
   status_code INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS outgoing_paths (
   transfer_id TEXT NOT NULL,
   path TEXT NOT NULL,
   bytes INT NOT NULL, 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS incoming_paths (
   transfer_id TEXT NOT NULL,   
   path TEXT NOT NULL, 
   bytes INT NOT NULL, 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -62,14 +62,14 @@ CREATE TABLE IF NOT EXISTS incoming_paths (
 CREATE TABLE IF NOT EXISTS outgoing_path_pending_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS outgoing_path_started_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL, 
   bytes_sent INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS outgoing_path_cancel_states (
   path_id INTEGER NOT NULL, 
   by_peer INTEGER NOT NULL,
   bytes_sent INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
@@ -87,14 +87,14 @@ CREATE TABLE IF NOT EXISTS outgoing_path_failed_states (
   path_id INTEGER NOT NULL, 
   status_code INTEGER NOT NULL,
   bytes_sent INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_sent >= 0)
 );
 CREATE TABLE IF NOT EXISTS outgoing_path_completed_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL,   
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES outgoing_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -102,14 +102,14 @@ CREATE TABLE IF NOT EXISTS outgoing_path_completed_states (
 CREATE TABLE IF NOT EXISTS incoming_path_pending_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL, 
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES incoming_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS incoming_path_started_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL, 
   bytes_received INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES incoming_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_received >= 0)
 );
@@ -118,7 +118,7 @@ CREATE TABLE IF NOT EXISTS incoming_path_cancel_states (
   path_id INTEGER NOT NULL,
   by_peer INTEGER NOT NULL,
   bytes_received INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES incoming_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_received >= 0)
 );
@@ -127,7 +127,7 @@ CREATE TABLE IF NOT EXISTS incoming_path_failed_states (
   path_id INTEGER NOT NULL, 
   status_code INTEGER NOT NULL,
   bytes_received INTEGER NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES incoming_paths(id) ON DELETE CASCADE ON UPDATE CASCADE,
   CHECK(bytes_received >= 0)
 );
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS incoming_path_completed_states (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   path_id INTEGER NOT NULL,   
   final_path TEXT NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+  created_at TIMESTAMP NOT NULL DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
   FOREIGN KEY(path_id) REFERENCES incoming_paths(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
