b3ddcce01787cf0d9f5e5759ca651db2e1156fca
Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>
2023-05-26 11:41:33 +0300

    remove sqlx temporairly
    Signed-off-by: Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>

--- drop-storage/src/lib.rs
+++ drop-storage/src/lib.rs
@@ -1,35 +1,36 @@
-use std::str::FromStr;
+// use std::str::FromStr;
 pub mod error;
 use slog::Logger;
-use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 
+// use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
 use crate::error::Error;
 
 type Result<T> = std::result::Result<T, Error>;
 // SQLite storage wrapper
 pub struct Storage {
     _logger: Logger,
-    #[allow(dead_code)]
-    conn: SqlitePool,
+    // #[allow(dead_code)]
+    // conn: SqlitePool,
 }
 
 impl Storage {
-    pub async fn new(logger: Logger, path: &str) -> Result<Self> {
-        let options = SqliteConnectOptions::from_str(path)?;
-        let options = options.create_if_missing(true);
+    pub async fn new(logger: Logger, _path: &str) -> Result<Self> {
+        Ok(Self { _logger: logger })
+        // let options = SqliteConnectOptions::from_str(path)?;
+        // let options = options.create_if_missing(true);
 
-        let conn = SqlitePool::connect_with(options)
-            .await
-            .map_err(|e| error::Error::InternalError(e.to_string()))?;
+        // let conn = SqlitePool::connect_with(options)
+        //     .await
+        //     .map_err(|e| error::Error::InternalError(e.to_string()))?;
 
-        sqlx::migrate!("./migrations")
-            .run(&conn)
-            .await
-            .map_err(|e| error::Error::InternalError(e.to_string()))?;
+        // sqlx::migrate!("./migrations")
+        //     .run(&conn)
+        //     .await
+        //     .map_err(|e| error::Error::InternalError(e.to_string()))?;
 
-        Ok(Self {
-            _logger: logger,
-            conn,
-        })
+        // Ok(Self {
+        //     _logger: logger,
+        //     conn,
+        // })
     }
 }
