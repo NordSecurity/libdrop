297bb688ee1558ff3afb5c4319faa38163cc64d4
Lukas Pukenis <lukas.pukenis@nordsec.com>
2023-06-30 10:58:18 +0300

    Fix FFI docs
    Signed-off-by: Lukas Pukenis <lukas.pukenis@nordsec.com>

--- norddrop/src/ffi/version.rs
+++ norddrop/src/ffi/version.rs
@@ -2,9 +2,11 @@ use libc::c_char;
 
 static VERSION: &[u8] = concat!(env!("DROP_VERSION"), "\0").as_bytes();
 
-/// @brief Get the version of the library
+/// Get the version of the library
 ///
-/// @return const char*
+/// # Returns
+/// 
+/// Version string
 #[no_mangle]
 pub extern "C" fn norddrop_version() -> *const c_char {
     VERSION.as_ptr() as *const _
