ae334fa49e5db14fd1396c79c0fc817121d96e95
Mateusz S. Szczygieł <mateusz.szczygiel@nordsec.com>
2023-07-25 13:29:59 +0300

    Apply suggestions from code review
    Co-authored-by: Martynas Gurskas <martynas.gurskas@nordsec.com>
    Signed-off-by: Mateusz Szczygieł <mateusz.szczygiel@nordsec.com>

--- drop-storage/src/sync.rs
+++ drop-storage/src/sync.rs
@@ -220,13 +220,14 @@ pub(super) fn outgoing_file_set_local_state(
 
     let count = conn.execute(
         r#"
-        UPDATE sync_outgoing_files sof
-        SET sof.local_state = ?3
-        WHERE sof.sync_id, sof.path_id IN (
-            SELECT st.sync_id, op.id
-            FROM sync_transfer st
+        UPDATE sync_outgoing_files
+        SET local_state = ?3
+        WHERE ROWID IN (
+            SELECT sof.ROWID
+            FROM sync_outgoing_files sof
+            INNER JOIN sync_transfer st USING(sync_id)
             INNER JOIN transfers t ON t.id = st.transfer_id
-            INNER JOIN outgoing_paths op ON t.id = op.transfer_id
+            INNER JOIN outgoing_paths op ON t.id = op.transfer_id AND sof.path_id = op.id
             WHERE st.transfer_id = ?1 AND op.path_hash = ?2
         )
         "#,
@@ -245,13 +246,14 @@ pub(super) fn outgoing_file_set_remote_state(
 
     let count = conn.execute(
         r#"
-        UPDATE sync_outgoing_files sof
-        SET sof.remote_state = ?3
-        WHERE sof.sync_id, sof.path_id IN (
-            SELECT st.sync_id, op.id
-            FROM sync_transfer st
+        UPDATE sync_outgoing_files
+        SET remote_state = ?3
+        WHERE ROWID IN (
+            SELECT sof.ROWID
+            FROM sync_outgoing_files sof
+            INNER JOIN sync_transfer st USING(sync_id)
             INNER JOIN transfers t ON t.id = st.transfer_id
-            INNER JOIN outgoing_paths op ON t.id = op.transfer_id
+            INNER JOIN outgoing_paths op ON t.id = op.transfer_id AND sof.path_id = op.id
             WHERE st.transfer_id = ?1 AND op.path_hash = ?2
         )
         "#,
@@ -425,13 +427,14 @@ pub(super) fn incoming_file_set_local_state(
 
     let count = conn.execute(
         r#"
-        UPDATE sync_incoming_files sif
-        SET sif.local_state = ?3
-        WHERE sif.sync_id, sif.path_id IN (
-            SELECT st.sync_id, ip.id
-            FROM sync_transfer st
+        UPDATE sync_incoming_files
+        SET local_state = ?3
+        WHERE ROWID IN (
+            SELECT sif.ROWID
+            FROM sync_incoming_files sif
+            INNER JOIN sync_transfer st USING(sync_id)
             INNER JOIN transfers t ON t.id = st.transfer_id
-            INNER JOIN incoming_paths ip ON t.id = ip.transfer_id
+            INNER JOIN incoming_paths ip ON t.id = ip.transfer_id AND sif.path_id = ip.id
             WHERE st.transfer_id = ?1 AND ip.path_hash = ?2
         )
         "#,
@@ -450,13 +453,14 @@ pub(super) fn incoming_file_set_remote_state(
 
     let count = conn.execute(
         r#"
-        UPDATE sync_incoming_files sif
-        SET sif.remote_state = ?3
-        WHERE sif.sync_id, sif.path_id IN (
-            SELECT st.sync_id, ip.id
-            FROM sync_transfer st
+        UPDATE sync_incoming_files
+        SET remote_state = ?3
+        WHERE ROWID IN (
+            SELECT sif.ROWID
+            FROM sync_incoming_files sif
+            INNER JOIN sync_transfer st USING(sync_id)
             INNER JOIN transfers t ON t.id = st.transfer_id
-            INNER JOIN incoming_paths ip ON t.id = ip.transfer_id
+            INNER JOIN incoming_paths ip ON t.id = ip.transfer_id AND sif.path_id = ip.id
             WHERE st.transfer_id = ?1 AND ip.path_hash = ?2
         )
         "#,
