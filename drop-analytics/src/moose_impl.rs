20fa52e36d455aa19c25ee7a48d0494b7976e5cb
Martynas Gurskas <martynas.gurskas@nordsec.com>
2023-10-06 11:16:50 +0300

    Bump moose tracker version
    Signed-off-by: Martynas Gurskas <martynas.gurskas@nordsec.com>

--- drop-analytics/src/moose_impl.rs
+++ drop-analytics/src/moose_impl.rs
@@ -14,7 +14,7 @@ const DROP_MOOSE_APP_NAME: &str = "norddrop";
 
 /// Version of the tracker used, should be updated every time the tracker
 /// library is updated
-const DROP_MOOSE_TRACKER_VERSION: &str = "1.0.1";
+const DROP_MOOSE_TRACKER_VERSION: &str = "2.0.0";
 
 pub struct MooseImpl {
     logger: slog::Logger,
