16d8911dc01a219dcb66283b9e3e8718bbfbe0e8
Daniel Fetti <daniel.fetti@nordsec.com>
2024-04-05 03:03:31 +0300

    Add checksum_events_granularity config
    The problem consists in the fact that while transferring files
    libdrop will generate ChecksumProgress events for every chunk of data
    received (256KB) which can be a lot if the file is large.
    
    Setting the checksum_events_granularity config makes the event
    generation to occur at every checksum_events_granularity bytes of data
    processed.
    
    Signed-off-by: Daniel Fetti <daniel.fetti@nordsec.com>

--- drop-transfer/src/ws/client/v6.rs
+++ drop-transfer/src/ws/client/v6.rs
@@ -172,6 +172,7 @@ impl HandlerLoop<'_> {
                     .checksum::<_, futures::future::Ready<()>>(
                         limit,
                         None::<fn(u64) -> futures::future::Ready<()>>,
+                        None,
                     )
                     .await?;
 
