8a45839aac8416f5362d46f6c3915c126a05486f
Lukas Pukenis <lukas.pukenis@nordsec.com>
2025-02-28 10:01:17 +0200

    Remove the migration for incoming_path_pending_states
    macOS 11.5 has prebundled sqlite3 - 3.32.3 which we
    need to support.
    
    There are two features we use in 003 and 004 migrations that
    is not supported there:
    
    1. Use of `UPDATE-FROM` syntax which
    is only supported since SQLite 2020-08-14 (3.33.0).
    
    2. Use of ALTER TABLE DROP COLUMN which is
    only supported since SQLite 2021-03-12 (3.35.0).
    
    The 003 migration was introduced in v5.0.1 and 004 was
    introduced in v5.1.0 both are used anymore by any platform
    (earliest used v6.3.0 at the moment of writing). Meaning
    the effort to migrate properly the UPDATE FROM is not worth it.
    
    Signed-off-by: Lukas Pukenis <lukas.pukenis@nordsec.com>

--- drop-storage/migrations/004-sync-cleanup/up.sql
+++ drop-storage/migrations/004-sync-cleanup/up.sql
@@ -1,4 +1,17 @@
 -- Add migration script here
+--- todo: the ALTER TABLE DROP COLUMN is only supported since 2021-03-12 (3.35.0)
+--- ALTER TABLE sync_transfer DROP COLUMN remote_state;
 
-ALTER TABLE sync_transfer DROP COLUMN remote_state;
+CREATE TABLE IF NOT EXISTS sync_transfer_new (
+  sync_id INTEGER PRIMARY KEY AUTOINCREMENT, -- use separate primary key for cascade to work across sync_ tables
+  transfer_id TEXT NOT NULL,
+  local_state INTEGER NOT NULL,
+  FOREIGN KEY(transfer_id) REFERENCES transfers(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO sync_transfer_new (sync_id, transfer_id, local_state) SELECT sync_id, transfer_id, local_state FROM sync_transfer;
+
+DROP TABLE sync_transfer;
+
+ALTER TABLE sync_transfer_new RENAME TO sync_transfer;
 
